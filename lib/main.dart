47af199d8f267e0a2fd97f93a0f119871d815293
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-03-08 12:02:15 +0100

    Remove unused import and comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sandbox_practice_flutter/animations/implicit/smooth_loading_indicator.dart';
-import 'package:sandbox_practice_flutter/animations/implicit/smooth_text_progress.dart';
 
 import 'cards/animated_progress_indicator.dart';
 
@@ -25,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
